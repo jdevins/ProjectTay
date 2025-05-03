cacd82b113533c54f2f8f05f2229d6ae836cee82
jdevins <jdevinstiffler@gmail.com>
2025-05-03 00:58:54 -0400

    Load ENVVARS on start

---
+++ src/auth/utilities/env_helper.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import log from './logging.js'; // Explicitly import the logger
+
+export function initializeEnv(envFilePath) {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const envPath = path.resolve(__dirname, envFilePath);
+    dotenv.config({ path: envPath });
+    log.info(`Environment variables initialized.`);
+}
